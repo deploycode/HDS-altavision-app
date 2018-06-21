5b582aff0b4a122d9da3dd9cbe7dfd33aeb40814
Gabriel Rodríguez <ggarcia@optimeconsulting.com>
2018-06-21 13:19:55 -0500

    mensaje de retorno version original

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -10,14 +10,12 @@
         <tbody>
           <td>{{$firstmenu->name}}</td>
           <td>
-            {!!Form::open(['route'=>['firstmenu.destroy',$firstmenu->id], 'method'=>'DELETE' ,'id'=>'delete_form'])!!}
-            <div class="form-group">
-              <a href="{{ route('firstmenu.edit', $firstmenu->id) }}">
-            <i class="btn btn-primary glyphicon glyphicon-pencil"></i></a>
-              {!! Form::button('<span class="glyphicon glyphicon-trash"></span>', ['class'=>'btn btn-danger' ,
-              'onclick' => 'if (confirm("¿Esta seguro de eliminar este elemento?")){ document.getElementById("delete_form").submit(); }' ]) !!}
-            </div>
-            {!!Form::close()!!}
+              {!!Form::open(['route'=>['firstmenu.destroy',$firstmenu->id], 'method'=>'DELETE'])!!}
+              <div class="form-group">
+                  <a href="{{ route('firstmenu.edit', $firstmenu->id) }}"><i class="btn btn-primary glyphicon glyphicon-pencil"></i></a>
+                  {!! Form::button('<span class="glyphicon glyphicon-trash"></span>', ['type' => 'submit', 'class'=>'btn btn-danger']) !!}
+              </div>
+              {!!Form::close()!!}
           </td>
         </tbody>
       @endforeach
